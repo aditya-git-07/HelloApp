3722038bc2819d7b9d09e840bc03970e56c51112
Aditya Nandan <225592552+aditya-git-07@users.noreply.github.com>
2026-03-11 10:49:28 +0530

    Delete OneDrive/Desktop/programing c directory

--- OneDrive/Desktop/programing c/college assgn/t3.c
+++
@@ -1,9 +0,0 @@
- #include <stdio.h> 
-int main()
-{
-    char num = '10';
-  
-    printf("%d", num);
-  
-    return 0;
- 
--- OneDrive/Desktop/programing c/shortcut.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("hello world from shortcut.c\n");
-    printf("\nThis is the second line");
-    printf("This is the third line");
-    printf("nn\n\nnn\n");
-    printf("nn/n/nnn/n");
-    return 0;
-}
